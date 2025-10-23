04e261d5be6a6c8ddbb049c15a554eb78cf7a5ac
GemedetAdept <vivian.zitek@gmail.com>
2025-10-23 16:57:00 -0600

    Create base classes for each color model in Convert

--- src/Convert.cs
+++ src/Convert.cs
@@ -1,5 +1,22 @@
 namespace ChromaToast.Color {
+
 	public class Convert {
-		internal Convert() {}
-}
+		internal static Convert() { }
+
+		public class RGB {
+			internal static RGB() { }
+		}
+
+		public class HSV {
+			internal static HSV() { }
+		}
+
+		public class HSL {
+			internal static HSL() { }
+		}
+
+		public class HEX {
+			internal static HEX() { }
+		}
+	}
 }
