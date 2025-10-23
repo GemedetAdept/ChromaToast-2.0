9dee6dc77216ad2b95a5be4eeb50defbe5d33cfe
Vivian Zitek <vivian.zitek@gmail.com>
2025-10-23 10:07:51 -0600

    Add RGB class and constructor to Convert class

--- src/Convert.cs
+++ src/Convert.cs
@@ -1,5 +1,17 @@
 namespace ChromaToast.Convert {
 	public class Convert {
 		internal Convert() {}
+
+		public class RGB {
+			public int Red {get; set;}
+			public int Green {get; set;}
+			public int Blue {get; set;}
+
+			public static RGB(int red, int green, int blue) {
+				Red = red;
+				Green = green;
+				Blue = blue;
+			}
+		}
 	}
 }
