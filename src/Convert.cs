8798630e3bbdacd917169a43d53533f160cfa87b
Vivian Zitek <vivian.zitek@gmail.com>
2025-10-23 12:13:58 -0600

    Remove RGB class from Convert in favor of separate Color class

--- src/Convert.cs
+++ src/Convert.cs
@@ -1,17 +1,4 @@
-namespace ChromaToast.Convert {
+namespace ChromaToast.Color {
 	public class Convert {
 		internal Convert() {}
-
-		public class RGB {
-			public int Red {get; set;}
-			public int Green {get; set;}
-			public int Blue {get; set;}
-
-			public static RGB(int red, int green, int blue) {
-				Red = red;
-				Green = green;
-				Blue = blue;
-			}
-		}
-	}
 }
