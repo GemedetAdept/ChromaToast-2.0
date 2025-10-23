41fe330fef72204f987cf2b8fc8268dc24a44b98
GemedetAdept <vivian.zitek@gmail.com>
2025-10-23 17:32:20 -0600

    Create RedValue_UnderFloor_ReturnsFloor test

--- tests/RGBTests.cs
+++ tests/RGBTests.cs
@@ -1,10 +1,19 @@
-using ChromaToast.Color
+using ChromaToast.Color;
 
 namespace ChromaToast.Tests {
     public class RGBTests {
 
         [Fact]
         public void RedValue_UnderFloor_ReturnsFloor() {
+			float redValue = -0.7f;
+			float greenValue = 1.0f;
+			float blueValue = 1.0f;
+			float expected = 0.0f;
+
+			var colorRGB = new Color.RGB(redValue, greenValue, blueValue);
+			float result = colorRGB.Red;
+
+			Assert.Equal(expected, result);
         }
     }
 }
