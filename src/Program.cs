9987514e762ff5f9bdf6349d0d89c057cf405a9c
Vivian Zitek <vivian.zitek@gmail.com>
2025-10-23 13:36:10 -0600

    Small test in main program to test value validation

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,5 @@
 using ChromaToast.Color;
 
-Color.RGB newRGB = new Color.RGB(red: 1.0f, green: 0.5f, blue: 0.5f);
+Color.RGB newRGB = new Color.RGB(red: 1.5f, green: -0.5f, blue: 0.5f);
+Color.HSV newHSV = new Color.HSV(hue: 1.5f, saturation: -0.5f, valueHSV: 0.5f);
+Color.HSL newHSL = new Color.HSL(hue: 1.5f, saturation: -0.5f, lightness: 0.5f);
--- src/obj/Debug/net9.0/ChromaToast.AssemblyInfo.cs
+++ src/obj/Debug/net9.0/ChromaToast.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChromaToast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d48cdd95c6a1666defb38844f86e65ea928610e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dee088aad1db44cd6d591afb405b1b73ef5799d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChromaToast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChromaToast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
