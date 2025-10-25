b74c517126790fad60dae4697112e8da57ef184c
GemedetAdept <vivian.zitek@gmail.com>
2025-10-25 08:19:47 -0600

    Add RedValue_AboveCeiling_ReturnsCeiling test

--- ChromaToast.Tests/UnitTest1.cs
+++ ChromaToast.Tests/UnitTest1.cs
@@ -15,5 +15,20 @@ namespace ChromaToast.Tests {
 
 			Assert.Equal(expected, result);
 		}
+
+		[Fact]
+		public void RedValue_AboveCeiling_ReturnsCeiling() {
+			
+			float valueRed = 1.7f;
+			float valueGreen = 1.0f;
+			float valueBlue = 1.0f;
+			float expected = 1.0f;
+
+			var colorRGB = new ChromaToast.Chroma.Chroma.RGB(valueRed, valueGreen,valueBlue);
+			float result = colorRGB.Red;
+
+			Assert.Equal(expected, result);
+		}
 	}
+
 }
