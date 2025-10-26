9bdda6168dda4550469a6e4a9dd29ed1ca69477c
GemedetAdept <vivian.zitek@gmail.com>
2025-10-26 12:56:36 -0600

    Add comment listing references for color space conversion equations

--- src/Convert.cs
+++ src/Convert.cs
@@ -1,6 +1,14 @@
 namespace ChromaToast.Chroma {
 
 	public class Convert {
+		/* Conversion equations referenced from:
+		 * Ford, A. and Roberts, A. (1998) Color Space Conversions
+		 * Travis, D. (1991) Effective Color Displays: Theory and Practice
+		 *		(Computers and People). Academic Press. DOI 10.1016/0003-6870(92)90258-W
+		 * Wikipedia - HSV and HSL
+		 *		(https://web.archive.org/web/20251012000154/https://en.wikipedia.org/wiki/HSL_and_HSV)
+		 * The Python Standard Library, colorsys.py
+		*/
 		static Convert() { }
 
 		public class RGB {
