4980498202bbed8ff6b2dc6986cd7e0a7ea03232
GemedetAdept <vivian.zitek@gmail.com>
2025-10-28 12:02:40 -0600

    Create Card.cs class for storing all versions of a color and for use in a future Palette class

---
+++ src/Card.cs
@@ -0,0 +1,15 @@
+﻿namespace ChromaToast {
+	public class Card { 
+		public Chroma.RGB RGB {get;set;}
+		public Chroma.HSV HSV {get;set;}
+		public Chroma.HSL HSL {get;set;}
+		public Chroma.HEX HEX {get;set;}
+
+		public Card() {
+			RGB = new Chroma.RGB(0.0f, 0.0f, 0.0f);	
+			HSV = new Chroma.HSV(0.0f, 0.0f, 0.0f);
+			HSL = new Chroma.HSL(0.0f, 0.0f, 0.0f);
+			HEX = new Chroma.HEX("000000");
+		}
+	}
+}
