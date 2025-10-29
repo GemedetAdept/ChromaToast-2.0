110e9cd77024fde854ec1f5588ebead5702d2c76
GemedetAdept <vivian.zitek@gmail.com>
2025-10-29 12:18:32 -0600

    Create OverwriteCards() method

--- src/Palette.cs
+++ src/Palette.cs
@@ -27,5 +27,51 @@ namespace ChromaToast {
 		public void AppendPalette(Palette palette) {
 			Cards.AddRange(palette.Cards);	
 		}
+
+		public void OverwriteCards(List<Card> cards, int[] indices) {
+			
+			// Array length mismatch
+			bool lengthsMatch = false;
+			try {
+				if (cards.Count != indices.Length) {
+					throw new ArrayLengthMismatchException();
+				}
+				else { lengthsMatch = true; }
+			}
+			catch (ArrayLengthMismatchException ex) {
+				Console.WriteLine(ex.Message + " Array lengths: {0}, {1}.",
+					cards.Count, indices.Length	
+				);
+			}
+
+			// Largest index mismatch
+			bool largestIndexMatches = false;
+			int largestIndex = Int32.MinValue;
+			try {
+				for (int i=0; i < indices.Length; i++) {
+					if (indices[i] > largestIndex) { largestIndex = indices[i]; }	
+				}
+
+				if (this.Cards.Count < largestIndex) {
+					throw new LargestIndexMismatchException();
+				}
+				else { largestIndexMatches = true; }
+			}
+
+			catch (LargestIndexMismatchException ex) {
+				Console.WriteLine(ex.Message + "Target array length: {0}. Largest index input: {1}",
+					this.Cards.Count, largestIndex	
+				);
+			}
+
+			if (lengthsMatch==true && largestIndexMatches==true) {
+				int indicesIter = 0;
+				for (int i=0; i < this.Cards.Count; i++) {
+					if (indices[indicesIter] == i) { this.Cards[i] = cards[indicesIter]; }	
+
+					indicesIter++;
+				}
+			}
+		}
 	}
 }
--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ChromaToast;
+using ChromaToast.Exceptions;
 
 // Value Clamping
 void clamping() {
