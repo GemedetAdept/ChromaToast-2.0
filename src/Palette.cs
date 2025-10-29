112267f156b29f87fe5d77ee396a5196c385130a
GemedetAdept <vivian.zitek@gmail.com>
2025-10-29 12:03:12 -0600

    Change name of ArrayIndexMismatchException.cs to ArrayLengthMismatchException.cs

--- src/Palette.cs
+++ src/Palette.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChromaToast.Exceptions;
 
 namespace ChromaToast {
 	internal class Palette {
