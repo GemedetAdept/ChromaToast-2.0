513e58e7c0fb6b59e69b66456d4c80978fa43747
GemedetAdept <vivian.zitek@gmail.com>
2025-10-30 13:15:07 -0600

    Create Harmony.Hue.Complementary() method

--- src/Harmony.cs
+++ src/Harmony.cs
@@ -7,7 +7,21 @@ using System.Threading.Tasks;
 namespace ChromaToast {
 	public class Harmony {
 		public class Hue {
+			
+			public Card[] Complementary(Card card0) {
+				Chroma.HSV hsv0 = card0.HSV;
+				float hue0 = hsv0.Hue;
 
+				Chroma.HSV hsv1 = hsv0;
+				float hue1 = Math.Abs((hue0 + 0.5f) - 1.0f);
+				hsv1.Hue = hue1;
+
+				Card card1 = new Card();
+				card1.InputChroma(hsv1);
+
+				Card[] output = new Card[] { card0, card1 };
+				return output;
+			}
 		}
 	}
 }
